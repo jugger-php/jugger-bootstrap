a5eb076723667e40e4a82d9d8ab988c421e859b4
irpsv <i.rpsv@live.com>
2017-02-18 00:17:06 +0500

    add Carousel realise

--- tests/bootstrap-carousel.php
+++ tests/bootstrap-carousel.php
@@ -1,16 +1,21 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use jugger\bootstrap\Carousel;
+use jugger\html\tag\Img;
+use jugger\html\EmptyTag;
 
 class CarouselTest extends TestCase
 {
     public function testDropdown()
     {
         $this->assertEquals(
-            new Carousel([
-                'id' => 'test',
-                'data' => [
-                    'ride' => 'carousel',
+            Carousel::widget([
+                'options' => [
+                    'id' => 'test',
+                    'data' => [
+                        'ride' => 'carousel',
+                    ],
                 ],
                 'items' => [
                     [
@@ -26,41 +31,34 @@ class CarouselTest extends TestCase
                     ]
                 ],
                 'indicators' => true,
+                'arrows' => false,
             ]),
-            "<div id='test' data-ride='carousel' class='carousel slide'>".
-            '<ol class="carousel-indicators">'.
-            '<li data-target="#test" data-slide-to="0" class="active"></li>'.
-            '<li data-target="#test" data-slide-to="1"></li>'.
-            '<li data-target="#test" data-slide-to="2"></li>'.
-            '</ol>'.
-            '<div class="carousel-inner" role="listbox">'.
-            '<div class="carousel-item active">'.
-            '<img class="d-block img-fluid" src="img-link.ext">'.
-            '</div>'.
-            '<div class="carousel-item">'.
-            '<img class="my-class" src="img-link.ext" alt="Carousel Item">'.
-            '</div>'.
-            '<div class="carousel-item">'.
-            '<img class="d-block img-fluid" src="img-link.ext">'.
-            '<div class="carousel-caption d-none d-md-block">'.
-            '<h3>Title</h3>'.
-            '<p>Content</p>'.
-            '</div>'.
-            '</div>'.
-            '</div>'.
-            '<a class="carousel-control-prev" href="#test" role="button" data-slide="prev">'.
-            '<span class="carousel-control-prev-icon" aria-hidden="true"></span>'.
-            '<span class="sr-only">Previous</span>'.
-            '</a>'.
-            '<a class="carousel-control-next" href="#test" role="button" data-slide="next">'.
-            '<span class="carousel-control-next-icon" aria-hidden="true"></span>'.
-            '<span class="sr-only">Next</span>'.
-            '</a>'.
+            "<div id='test' class='carousel slide' data-ride='carousel'>".
+            "<ol class='carousel-indicators'>".
+            "<li data-target='#test' data-slide-to='0' class='active'></li>".
+            "<li data-target='#test' data-slide-to='1'></li>".
+            "<li data-target='#test' data-slide-to='2'></li>".
+            "</ol>".
+            "<div class='carousel-inner' role='listbox'>".
+            "<div class='carousel-item active'>".
+            "<img class='d-block img-fluid' src='img-link.ext'>".
+            "</div>".
+            "<div class='carousel-item'>".
+            "<img class='my-class' alt='Carousel Item' src='img-link.ext'>".
+            "</div>".
+            "<div class='carousel-item'>".
+            "<img class='d-block img-fluid' src='img-link.ext'>".
+            "<div class='carousel-caption d-none d-md-block'>".
+            "<h3>Title</h3>".
+            "<p>Content</p>".
+            "</div>".
+            "</div>".
+            "</div>".
             "</div>"
         );
 
         $this->assertEquals(
-            new Carousel([
+            Carousel::widget([
                 'items' => [
                     [
                         'src' => 'img-link.ext',
@@ -72,27 +70,27 @@ class CarouselTest extends TestCase
                 ],
                 'indicators' => true,
             ]),
-            "<div id='carousel-id-1' data-ride='carousel' class='carousel slide'>".
-            '<ol class="carousel-indicators">'.
-            '<li data-target="#carousel-id-1" data-slide-to="0"></li>'.
-            '<li data-target="#carousel-id-1" data-slide-to="1" class="active"></li>'.
-            '</ol>'.
-            '<div class="carousel-inner" role="listbox">'.
-            '<div class="carousel-item">'.
-            '<img class="d-block img-fluid" src="img-link.ext">'.
-            '</div>'.
-            '<div class="carousel-item active">'.
-            '<img class="d-block img-fluid" src="img-link.ext">'.
-            '</div>'.
-            '</div>'.
-            '<a class="carousel-control-prev" href="#carousel-id-1" role="button" data-slide="prev">'.
-            '<span class="carousel-control-prev-icon" aria-hidden="true"></span>'.
-            '<span class="sr-only">Previous</span>'.
-            '</a>'.
-            '<a class="carousel-control-next" href="#carousel-id-1" role="button" data-slide="next">'.
-            '<span class="carousel-control-next-icon" aria-hidden="true"></span>'.
-            '<span class="sr-only">Next</span>'.
-            '</a>'.
+            "<div id='carousel-id-2' class='carousel slide' data-ride='carousel'>".
+            "<ol class='carousel-indicators'>".
+            "<li data-target='#carousel-id-2' data-slide-to='0'></li>".
+            "<li data-target='#carousel-id-2' data-slide-to='1' class='active'></li>".
+            "</ol>".
+            "<div class='carousel-inner' role='listbox'>".
+            "<div class='carousel-item'>".
+            "<img class='d-block img-fluid' src='img-link.ext'>".
+            "</div>".
+            "<div class='carousel-item active'>".
+            "<img class='d-block img-fluid' src='img-link.ext'>".
+            "</div>".
+            "</div>".
+            "<a class='carousel-control-prev' role='button' data-slide='prev' href='#carousel-id-2'>".
+            "<span class='carousel-control-prev-icon' aria-hidden='true'></span>".
+            "<span class='sr-only'>prev</span>".
+            "</a>".
+            "<a class='carousel-control-next' role='button' data-slide='next' href='#carousel-id-2'>".
+            "<span class='carousel-control-next-icon' aria-hidden='true'></span>".
+            "<span class='sr-only'>next</span>".
+            "</a>".
             "</div>"
         );
     }
