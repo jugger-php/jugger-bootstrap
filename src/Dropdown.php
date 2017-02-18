c40d79e8fadafb43f5573b0c410cdbc1ea0b4be7
irpsv <i.rpsv@live.com>
2017-02-18 12:21:53 +0500

    add Dropdown

--- src/Dropdown.php
+++ src/Dropdown.php
@@ -4,28 +4,30 @@ namespace jugger\bootstrap;
 
 use jugger\ui\Widget;
 use jugger\html\ContentTag;
-use jugger\html\tag\P;
+use jugger\html\tag\H6;
 use jugger\html\tag\Div;
+use jugger\html\tag\Link;
 use jugger\html\tag\Button;
 
-class Alert extends Widget
+class Dropdown extends Widget
 {
-    public $type;
-    public $content;
+    public $split;
+    public $right;
+    public $items = [];
+    public $button;
     public $header;
-    public $dismiss;
+    public $dropup;
     public $options = [];
 
     public function init()
     {
         $options = [
-            'class' => 'alert',
-            'role' => 'alert',
+            'class' => 'dropdown',
         ];
-        if ($this->type) {
-            $options['class'] .= " alert-{$this->type}";
-        }
 
+        if ($this->dropup) {
+            $options['class'] .= ' dropup';
+        }
         $this->options = array_merge($options, $this->options);
     }
 
@@ -33,9 +35,9 @@ class Alert extends Widget
     {
         $tag = new Div('', $this->options);
         $childs = [
-            $this->getDismissTag(),
-            $this->getHeaderTag(),
-            $this->getContentTag(),
+            $this->getButtonTag(),
+            $this->getSplitTag(),
+            $this->getMenuTag(),
         ];
         foreach ($childs as $child) {
             if ($child) {
@@ -46,43 +48,97 @@ class Alert extends Widget
         return $tag->render();
     }
 
-    public function getDismissTag()
+    public function getButtonTag()
     {
-        if (!$this->dismiss) {
+        if ($this->button instanceof ContentTag) {
+            $tag = $this->button;
+        }
+        else {
+            $tag = new Button($this->button, [
+                'type' => 'button',
+                'class' => 'btn btn-secondary',
+            ]);
+        }
+
+        if ($this->split) {
+            return $tag;
+        }
+        else {
+            return $this->setToggleOptions($tag);
+        }
+    }
+
+    public function getSplitTag()
+    {
+        if (!$this->split) {
             return null;
         }
 
-        $tag = new Button('<span aria-hidden="true">&times;</span>', [
-            'type' => 'button',
-            'class' => 'close',
+        $tag = new SecondaryButton("<span class='sr-only'>Toggle Dropdown</span>");
+        $tag->class .= " dropdown-toggle-split";
+        return $this->setToggleOptions($tag);
+    }
+
+    public function setToggleOptions(ContentTag $tag)
+    {
+        foreach ($this->getButtonOptions() as $name => $value) {
+            if ($name == 'class') {
+                $tag->class .= " {$value}";
+            }
+            else {
+                $tag->$name = $value;
+            }
+        }
+        return $tag;
+    }
+
+    public function getButtonOptions()
+    {
+        return [
+            'class' => 'dropdown-toggle',
             'data' => [
-                'dismiss' => 'alert',
+                'toggle' => 'dropdown',
             ],
             'aria' => [
-                'label' => 'Close',
+                'haspopup' => true,
+                'expanded' => false,
             ],
-        ]);
-        return $tag;
+        ];
     }
 
-    public function getHeaderTag()
+    public function getMenuTag()
     {
-        if (!$this->header) {
-            return null;
-        }
-
-        $tag = new ContentTag('h4', $this->header, [
-            'class' => 'alert-heading',
+        $tag = new Div('', [
+            'class' => 'dropdown-menu',
         ]);
-        return $tag;
-    }
-
-    public function getContentTag()
-    {
-        if (!$this->content) {
-            return null;
+        if ($this->right) {
+            $tag->class .= " dropdown-menu-right";
+        }
+        if ($this->header) {
+            $header = new H6($this->header, [
+                'class' => 'dropdown-header',
+            ]);
+            $tag->add($header);
         }
 
-        return new P($this->content);
+        foreach ($this->items as $item) {
+            if (empty($item)) {
+                $divider = new Div('', [
+                    'class' => 'dropdown-divider',
+                ]);
+                $tag->add($divider);
+                continue;
+            }
+            elseif ($item instanceof Link) {
+                $child = $item;
+            }
+            else {
+                $child = new Link($item);
+            }
+
+            $child->class = 'dropdown-item';
+            $tag->add($child);
+        }
+        return $tag;
     }
 }
--- tests/bootstrap-dropdown.php
+++ tests/bootstrap-dropdown.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use jugger\bootstrap\Dropdown;
+use jugger\html\tag\Link;
 
 class DropdownTest extends TestCase
 {
@@ -9,8 +10,10 @@ class DropdownTest extends TestCase
     {
         $this->assertEquals(
             Dropdown::widget([
-                'id' => 'test',
-                'class' => 'dropdown show',
+                'options' => [
+                    'id' => 'test',
+                    'class' => 'dropdown show',
+                ],
                 'button' => 'Test Label',
                 'header' => 'Header Items',
                 'items' => [
@@ -20,39 +23,39 @@ class DropdownTest extends TestCase
                     new Link('Link3'),
                 ],
             ]),
-            "<div id='test' class='dropdown show'>".
+            "<div class='dropdown show' id='test'>".
             "<button type='button' class='btn btn-secondary dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Test Label</button>".
             "<div class='dropdown-menu'>".
             "<h6 class='dropdown-header'>Header Items</h6>".
-            "<a class='dropdown-item' href='#'><span>Link1</span></a>".
-            "<a class='dropdown-item' href='#'>Link2</a>".
+            "<a href='#' class='dropdown-item'><span>Link1</span></a>".
+            "<a href='#' class='dropdown-item'>Link2</a>".
             "<div class='dropdown-divider'></div>".
-            "<a class='dropdown-item' href='#'>Link3</a>".
+            "<a href='#' class='dropdown-item'>Link3</a>".
             "</div>".
             "</div>"
         );
 
         $this->assertEquals(
             Dropdown::widget([
-                'align-right' => true,
+                'right' => true,
                 'dropup' => true,
                 'split' => true,
                 'button' => new Link('Test Label', '#', [
                     'class' => 'btn btn-secondary',
                 ]),
                 'items' => [
-                    new Link('Test1'),
-                    new Link('Test2'),
-                    new Link('Test3'),
+                    new Link('Link1'),
+                    new Link('Link2'),
+                    new Link('Link3'),
                 ],
             ]),
             "<div class='dropdown dropup'>".
-            "<a href='#' class='btn btn-secondary'>Test Label</a>".
-            "<button type='button' class='btn btn-secondary dropdown-toggle dropdown-toggle-split' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'><span class='sr-only'>Toggle Dropdown</span></button>".
+            "<a class='btn btn-secondary' href='#'>Test Label</a>".
+            "<button class='btn btn-secondary dropdown-toggle-split dropdown-toggle' type='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'><span class='sr-only'>Toggle Dropdown</span></button>".
             "<div class='dropdown-menu dropdown-menu-right'>".
-            "<a class='dropdown-item' href='#'>Link1</a>".
-            "<a class='dropdown-item' href='#'>Link2</a>".
-            "<a class='dropdown-item' href='#'>Link3</a>".
+            "<a href='#' class='dropdown-item'>Link1</a>".
+            "<a href='#' class='dropdown-item'>Link2</a>".
+            "<a href='#' class='dropdown-item'>Link3</a>".
             "</div>".
             "</div>"
         );
