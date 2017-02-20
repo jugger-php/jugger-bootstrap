5f1b2610fd61ff47b1cc757c74ec9f1d78bcfda0
irpsv <i.rpsv@live.com>
2017-02-20 20:47:18 +0500

    add Nav

--- tests/bootstrap-nav.php
+++ tests/bootstrap-nav.php
@@ -1,14 +1,19 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use jugger\html\tag\Link;
+use jugger\bootstrap\Nav;
+use jugger\bootstrap\Dropdown;
 
 class NavTest extends TestCase
 {
     public function testDropdown()
     {
         $this->assertEquals(
-            new Nav([
-                'id' => 'test',
+            Nav::widget([
+                'options' => [
+                    'id' => 'test',
+                ],
                 'items' => [
                     new Link('Test1'),
                     [
@@ -20,8 +25,8 @@ class NavTest extends TestCase
                         'disabled' => true,
                     ],
                     [
-                        'content' => new Link('Test4'),
-                        'dropdown' => new Dropdown([
+                        'content' => new Dropdown([
+                            'button' => new Link('Test4'),
                             'items' => [
                                 new Link('Link1'),
                                 new Link('Link2'),
@@ -31,30 +36,32 @@ class NavTest extends TestCase
                     ],
                 ],
             ]),
-            "<ul id='test' class='nav'>".
+            "<ul class='nav' id='test'>".
             "<li class='nav-item'>".
-            "<a class='nav-link' href='#'>Test1</a>".
+            "<a href='#'>Test1</a>".
             "</li>".
             "<li class='nav-item'>".
-            "<a class='nav-link active' href='#'>Test2</a>".
+            "<a href='#' class=' nav-link active'>Test2</a>".
             "</li>".
             "<li class='nav-item'>".
-            "<a class='nav-link disabled' href='#'>Test3</a>".
+            "<a href='#' class=' nav-link disabled'>Test3</a>".
             "</li>".
             "<li class='nav-item dropdown'>".
-            "<a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Test4</a>".
+            "<a href='#' class=' nav-link dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Test4</a>".
             "<div class='dropdown-menu'>".
-            "<a class='dropdown-item' href='#'>Link1</a>".
-            "<a class='dropdown-item' href='#'>Link2</a>".
-            "<a class='dropdown-item' href='#'>Link3</a>".
+            "<a href='#' class='dropdown-item'>Link1</a>".
+            "<a href='#' class='dropdown-item'>Link2</a>".
+            "<a href='#' class='dropdown-item'>Link3</a>".
             "</div>".
             "</li>".
             "</ul>"
         );
 
         $this->assertEquals(
-            new Nav([
-                'class' => 'nav nav-tabs',
+            Nav::widget([
+                'options' => [
+                    'class' => 'nav nav-tabs',
+                ],
             ]),
             "<ul class='nav nav-tabs'></ul>"
         );
